4921c21e66fcc19a9eed097aeb20abf100307c75
Ben Klosky <benklosky@uchicago.edu>
2024-04-02 19:10:18 +0000

    functioning graph :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 // rename the file App.jsx
 // delete the React import
 import { createRoot } from "react-dom/client";
+import ForceGraph3D from 'react-force-graph-3d';
 
 const App = () => {
   return (
     <div>
-      <h1>Ben Klosky</h1>
+      <ForceGraph3D
+        graphData={
+            {
+                nodes: [
+                    { id: 1, name: "Node 1" },
+                    { id: 2, name: "Node 2" },
+                    { id: 3, name: "Node 3" },
+                    { id: 4, name: "Node 4" },
+                    { id: 5, name: "Node 5" },
+
+                ],
+                links: [
+                    { source: 1, target: 2 },
+                    { source: 1, target: 3 },
+                    { source: 1, target: 4 },
+                    { source: 1, target: 5 },
+
+                ],
+            }
+        }
+      />
     </div>
   );
 };
