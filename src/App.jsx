e43b97ab75705d2f55ab54a74b00bebba4a2f4a2
Ben Klosky <benklosky@uchicago.edu>
2024-04-03 03:17:34 +0000

    trying to make a div come in on node click

--- src/App.jsx
+++ src/App.jsx
@@ -13,38 +13,48 @@ const App = () => {
   window.addEventListener('resize', () => {
     setDisplayWidth(window.innerWidth);
     setDisplayHeight(window.innerHeight);
+    }
+  );
 
-});
+  // on node click, make a div fly in :0
+  var handleNodeClick = function(node){
+    console.log("ALERT SOME MF CLICKED A NODE UP IN HERE")
+  }
+
   return (
     <div>
-      <ForceGraph2D
-        graphData={
-            {
-                nodes: [
-                    { id: 1, name: "Node 1" },
-                    { id: 2, name: "Node 2" },
-                    { id: 3, name: "Node 3" },
-                    { id: 4, name: "Node 4" },
-                    { id: 5, name: "Node 5" },
+      <div id='mainbox'>
+        <div id='graph'>
+          <ForceGraph2D
+            graphData={
+                {
+                    nodes: [
+                        { id: 1, name: "Node 1" },
+                        { id: 2, name: "Node 2" },
+                        { id: 3, name: "Node 3" },
+                        { id: 4, name: "Node 4" },
+                        { id: 5, name: "Node 5" },
 
-                ],
-                links: [
-                    { source: 1, target: 2 },
-                    { source: 1, target: 3 },
-                    { source: 2, target: 4 },
-                    { source: 1, target: 5 },
+                    ],
+                    links: [
+                        { source: 1, target: 2 },
+                        { source: 1, target: 3 },
+                        { source: 2, target: 4 },
+                        { source: 1, target: 5 },
 
-                ],
+                    ],
+                }
             }
-        }
-        backgroundColor="#FFFFF0"
-        linkColor={() => '#626255'}
-        nodeAutoColorBy={"id"}
-        nodeRelSize={6}
-        width={displayWidth}
-        height={displayHeight}
-      />
+            backgroundColor="#2b2b2b"
+            linkColor={() => '#626255'}
+            nodeAutoColorBy={"id"}
+            nodeRelSize={7}
+            width={displayWidth}
+            height={displayHeight}
+          />
+      </div>
     </div>
+  </div>
   );
 };
 
